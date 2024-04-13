6d6cfaf1084ef1ecabe0783389e91dde57e71fa7
ali ghorbani <ag7055408@gmail.com>
2024-04-13 20:50:02 +0430

    create header component 25/1/1403 20:49

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { Header } from './pages/Components/Header';
+// import './css/fontAwesome5Pro.css';
+import './css/bootstrap.min.css';
+import './css/animate.min.css';
+import './css/backToTop.css';
+import './css/default.css';
+import './css/jquery.fancybox.min.css';
+import './css/meanmenu.css';
+import './css/nice-select.css';
+// import './css/owl.carousel.min.css';
+import './css/preloader.css';
+import './css/slick.css';
+// import './css/ui-range-slider.css';
+import './css/style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header></Header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
