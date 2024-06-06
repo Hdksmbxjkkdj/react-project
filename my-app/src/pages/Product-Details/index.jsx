27ec04a289f97b763b90f6dc1d16f15cf3580fc4
n.nasseri95 <n.nasseri1994@gmail.com>
2024-06-06 12:43:41 -0400

    Images on the page productdetails 12:42 1403.03.17

--- my-app/src/pages/Product-Details/index.jsx
+++ my-app/src/pages/Product-Details/index.jsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { ProductSlider } from '../Home1/best-selling/product-slider';
+// import { ProductSlider } from './ProductSlider';
 import { BrandSlider } from './BrandSlider';
 import { ModalContent } from './ModalContent';
 import { ProductDetailsDesTab } from './ProductDetailsDesTab';
@@ -6,54 +10,56 @@ import { ProductDetailsWrapper } from './ProductDetailsWrapper';
 import {ProducDetailsImg} from './ProductDetailsnav';
 import { SectionHead } from './SectionHead';
 import { TabContent } from './TabContent';
+import { BestSelling } from '../Home1/best-selling';
+export const ProducDetails= ({id=1}) =>{
+    const [info, setInfo] = useState();
+    useEffect(() => {
+        axios.get(`http://localhost:313/row/${id}`).then((res) => {
+            setInfo(res);
+           
 
-export const ProducDetails= () =>{
+        });
+    }, []);
     return<>
-    <main>
-    <section className="product__area box-plr-75 pb-70">
-    <div className="container-fluid">
-            <div className="row">
-                <ProducDetailsImg />
-                <ProductDetailsWrapper />
-            </div>
-            <div className="row">
-                <ProductDetailsDesTab />
-            </div>
-            
-            <div className="row">
-                 <TabContent />
-            </div>
-        </div>
-        </section>
-
-       
-        <section className="product__area box-plr-75 pb-20">
-            <div className="container-fluid">
-                <div className="row">
-                    <SectionHead />
+        <main>
+            <section className="product__area box-plr-75 pb-70">
+                <div className="container-fluid">
+                    <div className="row">
+                          <ProducDetailsImg item={info?.data?.pic} picturs={info?.data?.gallery} />
+                        <ProductDetailsWrapper />
+                    </div>
+                    <div className="row">
+                        <ProductDetailsDesTab />
+                    </div>
+                    
+                    <div className="row">
+                        <TabContent /> 
+                    </div>
                 </div>
-                <div className="row">
-                    <ProductSlider />
+            </section>
+            <section className="product__area box-plr-75 pb-20">
+                <div className="container-fluid">
+                    <div className="row">
+                        <SectionHead />
+                    </div>
+                    {/* <div className="row"> */}
+                    
+                    {/* </div> */}
                 </div>
+            </section>
+            <ProductSlider />
+            <section className="brand__area">
+                <div className="container custom-container">
+                    <div className="row align-items-center">
+                        <BrandSlider />
+                    </div>
+                </div>
+            </section>         
+            <div className="modal fade in" id="productModalId" tabindex="-1" role="dialog" aria-hidden="false">
+                    <div className="modal-dialog modal-dialog-centered product__modal" role="document">
+                        <ModalContent  />
+                    </div>
             </div>
-        </section>
-
-        <section className="brand__area">
-            <div className="container custom-container">
-                <div className="row align-items-center">
-                    <BrandSlider />
-                </div>
-            </div>
-         </section>
-
-
-                   
-        <div className="modal fade in" id="productModalId" tabindex="-1" role="dialog" aria-hidden="false">
-                <div className="modal-dialog modal-dialog-centered product__modal" role="document">
-                    <ModalContent  />
-                </div>
-        </div>
-    </main>
-   
+        </main>
     </>
 }
--- my-app/src/pages/Sidebar/SidebarData.jsx
+++ my-app/src/pages/Sidebar/SidebarData.jsx
@@ -1,19 +1,31 @@
-export const SidebarData=[]
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-for(let i=1;i <= 2;i++){
-    var id,price,img;
-    SidebarData.push(
-         {
+export const SidebarData=[]
+// for(let i=1;i <= 2;i++){
+//     var id,price,img;
+//     SidebarData.push(
+//          {
                     
        
-                 img:`https://picsum.photos/20${i}`,
-                 id:i,
-                 price:300+i,
-                 title:'Australian Large Granny Smith Apples',
-        }
+//                  img:`https://picsum.photos/20${i}`,
+//                  id:i,
+//                  price:300+i,
+//                  title:'Australian Large Granny Smith Apples',
+//         }
         
-    )
-    // console.log(SidebarData)
-    
+//     )
+//     // console.log(SidebarData)
+// console.log(slider)
+export const Test = () =>{
+    const [slider, setSlider] = useState();
+  useEffect(() => {
+    axios.get("http://localhost:313/best_selling").then((res) => {
+        setSlider(res);
+    });
+  }, []);
+  console.log(slider)
+    return<>
+    </>
+} 
 
-}
