d95d8fb56583a3114660f884946d3efbb7d9c76c
ali ghorbani <ag7055408@gmail.com>
2024-08-05 09:01:17 +0330

    last changing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,29 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Home1 } from "./pages/Home1/index";
-import { Menu } from "./pages/Components/Menu";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./pages/Components/Footer";
-import Layout from "./pages/Layout";
+import { Home1 } from "./pages/Home1/index";
 // import Breadcrumb from './pages/Product/breadcrumb';
 // import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Header } from "./pages/Components/Header";
-import Blog from "./pages/Blog";
-import BlogDetaile from "./pages/BlogDetailes";
+import { CartContext } from "./context/CardContext";
 import { AboutUs } from "./pages/AboutUs";
+import { Checkout } from "./pages/Auth/checkout";
 import { Login } from "./pages/Auth/login";
 import { Profile } from "./pages/Auth/profile";
 import { Register } from "./pages/Auth/register";
+import Blog from "./pages/Blog";
+import BlogDetaile from "./pages/BlogDetailes";
 import { Cart } from "./pages/Cart/cart";
-import { WishList } from "./pages/WishList/wishlist";
-import { Checkout } from "./pages/Auth/checkout";
-import { Error } from "./pages/Auth/404error";
+import { Header } from "./pages/Components/Header";
 import { BackToTop } from "./pages/Components/backtotop";
-import { CartContext } from "./context/CardContext";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import { ProducDetails } from "./pages/Product-Details/index";
+import { WishList } from "./pages/WishList/wishlist";
 
 // import Shop from './pages/Shop/Shop'
-import {Sidebar} from './pages/Sidebar/index';
-import { Productpage } from './pages/Productpage';
-import { BlogItem } from "./pages/Home1/best-selling/product-item/blog-item";
-import Show from "./pages/Show";
-import { Product } from "./pages/Shop/Product";
 import { ContactUs } from "./pages/ContactUs";
-import { Products } from "./pages/Shop/index";
+import { Productpage } from './pages/Productpage';
 
 function App() {
   window?.$(window).on("load", function () {
