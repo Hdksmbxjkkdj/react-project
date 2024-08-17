60baa4d0a5282dfacfbe52e7b0f2248cf8460fe5
ali ghorbani <ag7055408@gmail.com>
2024-08-17 09:13:30 +0330

    last commit in 27/5

--- my-app/src/pages/Home1/best-selling/product-item/product-item-lg.jsx
+++ my-app/src/pages/Home1/best-selling/product-item/product-item-lg.jsx
@@ -3,7 +3,7 @@ import { ProductThumb } from "./product-thumb";
 const ProductItemlg = (props)=> {
     return (
         <>
-      <div className="product__item white-bg">
+      <div className="product__item white-bg" key={Math.random()}>
         <ProductThumb item={props?.slider}></ProductThumb>
         <ProductContentlg item={props?.slider}></ProductContentlg>
       </div>
--- my-app/src/pages/Home1/best-selling/tab-content.jsx
+++ my-app/src/pages/Home1/best-selling/tab-content.jsx
@@ -1,10 +1,10 @@
 import { ProductSlider } from "./product-slider";
-const TabContent = ({type,res,item}) => {
+const TabContent = ({type,res,item,filter}) => {
   return (
     <>
       <div className="tab-content" id="best-sell">
         <div className="tab-pane fade show active" id="new" role="tabpanel" aria-labelledby="new-tab">
-          <ProductSlider type={type} res={res} item={item}></ProductSlider>
+          <ProductSlider type={type} res={res} item={item} filt={filter}></ProductSlider>
         </div>
       </div>
     </>
