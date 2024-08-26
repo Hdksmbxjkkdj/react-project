cc508db68b210e48b70db870dda975c8e88ba3a4
ali ghorbani <ag7055408@gmail.com>
2024-08-26 08:35:10 +0330

    adding new featuress 5/6/1403 8:35

---
+++ my-app/src/pages/Cart/UpdateCart.jsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export const UpdateCart = async({items})=> {
+    console.log(items);
+    // const newItems = {
+    //   "id": items.id,
+    //   "pic": items.pic,
+    //   "name": items.name,
+    //   "unitprice": items.unitprice,
+    //   "quantity": count,
+    //   "date": items.date
+    // }
+    // console.log("items");
+    // const res = await axios.put(`http://localhost:313/row/:${items?.id}`,items)
+    // const res = await axios.put(`http://localhost:313/row/:${items?.id}`,items)
+    // console.log(res);
+}
